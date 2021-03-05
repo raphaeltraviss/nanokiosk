e89d8cb628dcb1efa1809b06c98632a950a8574b
Raphael Traviss <raphael@skyleafdesign.com>
2021-03-05 14:00:45 -0600

    Desktop dev start in windowed mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <QtWidgets>
 #include <QDebug>
+#include <QCommandLineParser>
 #include "MainWindow.h"
 #include "BluetoothServer.h"
 
@@ -8,9 +9,31 @@
 int main(int argc, char *argv[])
 {
   QApplication app(argc, argv);
+  QCoreApplication::setApplicationName("nanokiosk");
+  QCoreApplication::setApplicationVersion("0.8.8");
+
+  QCommandLineParser parser;
+  parser.setApplicationDescription("Nanokiosk graphic terminal");
+  parser.addHelpOption();
+  parser.addVersionOption();
+
+
+  QCommandLineOption windowOption(QStringList() << "w" << "windowed",
+            QCoreApplication::translate("main", "Windowed mode for local dev"));
+  parser.addOption(windowOption);
+  parser.process(app);
+
+
   MainWindow the_window;
-  //the_window.showFullScreen();
-  the_window.show();
+
+  bool isWindowed = parser.isSet(windowOption);
+
+  if (isWindowed) {
+    the_window.show();
+  }
+  else {
+    the_window.showFullScreen();
+  }
 
   return app.exec();
 }
